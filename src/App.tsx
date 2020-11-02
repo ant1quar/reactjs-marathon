8696afe2323f1c960a6d2d295864f07709f3f401
Ivan Krivonos <ivan.krivonos@bitfury.com>
2020-11-02 22:29:58 +0300

    fix src

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import cn from 'classnames';
 import s from './App.module.scss';
+
 const App = () => {
-    return (
-        <div className={cn(s.header,'color')}>
-            This is div!
-        </div>
-    )
-}
+  return <div className={cn(s.header, 'color')}>This is div!</div>;
+};
 export default App;
