fcdcbbccc7b3aa1eba6cb125dc12f045d4175291
Ivan Krivonos <ivan.krivonos@bitfury.com>
2020-11-03 04:55:12 +0300

    update wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import cn from 'classnames';
 import s from './App.module.scss';
 import Footer from './components/footer';
 import Header from './components/header';
 
 const App = () => {
   return (
-    <>
+    <main className={s.main}>
       <Header />
-      <div className={cn(s.main)}>This is div!</div>
+      <div>content</div>
       <Footer />
-    </>
+    </main>
   );
 };
 export default App;
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import cn from 'classnames';
 import s from './style.module.scss';
 import HeaderMenu from '../header-menu';
 
 const Header = () => {
   return (
-    <header className={cn(s.header)}>
-      <div className={cn(s.wrapper)}>
-        <a href="/" className={cn(s.link)}>
-          <img src="public/img/logo.svg" className={cn(s.logo)} />
+    <header className={s.header}>
+      <div className={s.wrapper}>
+        <a href="/" className={s.link}>
+          <img src="public/img/logo.svg" className={s.logo} />
         </a>
         <HeaderMenu />
       </div>
